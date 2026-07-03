935d04f972da17a1c583677aced747b48c9f748e
Dhanush <111170398+DhanushPralayakaveri@users.noreply.github.com>
2026-07-03 16:52:07 +0530

    Add CORS configuration for frontend access

---
+++ uptime-backend/uptime_monitor/src/main/java/com/proj/uptimemonitor/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.proj.uptimemonitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "https://uptime-monitor-frontend-2hnfpg0vy-d-team3.vercel.app",
+                        "http://localhost:4200",
+                        "uptime-monitor-frontend-psi.vercel.app"
+                      
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
